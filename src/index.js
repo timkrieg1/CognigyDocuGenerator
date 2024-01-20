fe666fff5ad3e8226b3808c8aaa3b7ad2fda905e
timkrieg1 <155547583+timkrieg1@users.noreply.github.com>
2024-01-20 16:40:00 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-
+const startPDF = () => {
+    let doc = new docx.Document();
+  
+    doc.addSection({
+      children: [
+        new docx.Paragraph({
+          children: [
+            new docx.TextRun({
+              text: "Hi",
+            }),
+          ],
+        }),
+      ],
+    });
+    docx.Packer.toBlob(doc).then((blob) => {
+      saveAs(blob, "firstdoc.docx");
+    });
+  };
+  
+  document.addEventListener("click", startPDF);
+  
